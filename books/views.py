44989d856d4de206602f9ef9067a097b54a4ccb4
blaack007 <mohamedkhalifa.4053@gmail.com>
2025-06-17 12:57:00 +0300

    Add initial project setup with Django, static files, and base template
    - Created manage.py for Django command-line utility.
    - Added requirements.txt with necessary dependencies including Django, crispy forms, dotenv, and whitenoise.
    - Introduced CSS styles in static/css/styles.css for basic layout and form styling.
    - Developed base template in templates/base_generic.html with Bootstrap integration and navigation links for book management.

---
+++ books/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Book
+
+# Create your views here.
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/book_list.html' 
+    context_object_name = 'book_list'
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html' 
+    context_object_name = 'book'
+
+class BookCreateView(CreateView):
+    model = Book
+    template_name = 'books/book_form.html'
+    fields = ['title', 'author', 'publication_date', 'price'] 
+
+class BookUpdateView(UpdateView):
+    model = Book
+    template_name = 'books/book_form.html'
+    fields = ['title', 'author', 'publication_date', 'price'] 
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'books/book_confirm_delete.html'
+    success_url = reverse_lazy('book_list')
---
+++ bookstore/books/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('book_detail', args=[str(self.id)])
